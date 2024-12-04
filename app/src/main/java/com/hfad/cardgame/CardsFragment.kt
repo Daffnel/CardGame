2f02d8bb7d869ca598a9dd6cbcdf119b074b62bc
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2024-12-04 12:42:03 +0100

    Gameplay functions under progress

--- app/src/main/java/com/hfad/cardgame/CardsFragment.kt
+++ app/src/main/java/com/hfad/cardgame/CardsFragment.kt
@@ -23,6 +23,8 @@ override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
 viewModel = ViewModelProvider(this)[CardsViewModel::class.java]
+
+
 }
 
 
@@ -45,23 +47,25 @@ viewModel = ViewModelProvider(this)[CardsViewModel::class.java]
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        val image: Int = R.drawable.ace_of_hearts
-
-       //live  data to change imageview
 
 
-        binding.ivCard.setImageResource(image)
+       viewModel.currentCard.observe(viewLifecycleOwner, Observer { cardResId ->
+           cardResId?.let {
+               binding.ivCard.setImageResource(it)
+
+           }
+       })
+
+
 
         binding.btnLower.setOnClickListener {
-
-            // Lägg till funktionalitet för btnLower här
-            Toast.makeText(context, "Lower button clicked", Toast.LENGTH_SHORT).show()
+            viewModel.showCard()
+            viewModel.buttonLow()
         }
 
         binding.btnHigher.setOnClickListener {
-
-            // Lägg till funktionalitet för btnHigher här
-            Toast.makeText(context, "Higher button clicked", Toast.LENGTH_SHORT).show()
+            viewModel.buttonHigh()
+            viewModel.showCard()
         }
     }
 
--- app/src/main/java/com/hfad/cardgame/CardsViewModel.kt
+++ app/src/main/java/com/hfad/cardgame/CardsViewModel.kt
@@ -1,32 +1,80 @@
 package com.hfad.cardgame
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.random.Random
 
 class CardsViewModel: ViewModel() {
 
-    private lateinit var deck1: Cards
-    var cardCountNr: Int = 0 //  keep track which card is in play
+     val currentCard = MutableLiveData<Int>()
+    private val deck1 = Cards()
+    val cardImages = deck1.deckImageRef
+    val cards = deck1.randomizedDeck
 
-init{
-    var deck1: Cards = Cards()  //create a new shuffled deck
-    deck1.randomizeCards()
+    var cardCount = 0                       //kortleken startar på 0?
+
+    init {
+        deck1.randomizeCards()             //Skapa en balndad kortlek
+    }
+
+    fun showCard() {
+        currentCard.value = cardImages[cards.indexOf(cardCount)]
+        cardCount++
+
+        //Log.d("CardsViewModel","Current card is ${deck1.convertNumber(cards.indexOf(cardCount))}")
+
+    }
+
+    fun checkGuesses(answer: String): Boolean{  // HI = for high button LO= for low button Return Boolean for right or wrong answer
+
+       // val singleNumber = deck1.convertNumber(cards.indexOf(cardCount))[cardCount]
+
+        val firstValueToCompare = deck1.convertNumber(cards.indexOf(cardCount))
+        val secondValueToCompare = deck1.convertNumber(cards.indexOf(cardCount+1))
+
+        //val firstValueToCompare = deck1.convertNumber(cards.indexOf(cardCount))
+        //val secondValueToCompare = deck1.convertNumber(cards.indexOf(cardCount+1))
+
+        Log.d("CardsViewModel","cartd count $cardCount Första värdet $firstValueToCompare och andra värdet $secondValueToCompare")
+        Log.d("CardsViewModel","Svar $answer")
+        Log.d("CardsViewModel","{$cards.indexOf(cardCount+1)}")
+        Log.d("CardsViewModel","{$cards.indexOf(cardCount)}")
+        when(answer){
+
+            "HI" -> {if( secondValueToCompare > firstValueToCompare){
+                Log.d("CardsViewModel","Rätt gissat")
+                return true
+            } else {
+                Log.d("CardsViewModel","Fel gissat")
+            }}
+
+            "LO" -> {if( secondValueToCompare < firstValueToCompare){
+                Log.d("CardsViewModel","Rätt gissat")
+                return true
+            } else {
+                Log.d("CardsViewModel","Fel gissat")
+
+            }}
+
+            else -> {
+                Log.d("CardsViewModel","Här ska vu inte hamna")
+            }
+        }
+
+        return false
+    }
+
+    fun buttonHigh(){
+//Log.d("CardsViewModel","HI button tryckt")
+checkGuesses("HI")
+
+    }
+fun buttonLow(){
+//Log.d("CardsViewModel","LO button tryckt")
+    checkGuesses("LO")
 }
-
-
-    var currentImage = MutableLiveData<Int>()
-   val cardImages = deck1.deckImgaesRef
-
-
-
-
-/*fun showNextCard(){
-
-    currentImage.value = cardImages[cardCountNr]?.toInt()
-
-}*/
-
 
 
 }
